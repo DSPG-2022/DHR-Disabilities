3b111a1059ff2acea42a47f2b80100479c23b18a
joelmar0 <joelmar@iastate.edu>
2022-06-17 10:50:10 -0500

    Updated R script

--- Data Exploration/R Projects/IVRS_Data_Transformation.R
+++ Data Exploration/R Projects/IVRS_Data_Transformation.R
@@ -1,7 +1,8 @@
 library(tidyr)
 library(dplyr)
+library(stringr)
 
-IVRS_data <- read.csv("C:/Users/joelm/Documents/GitHub/DHR-Disabilities/Data Exploration/Datasets/Closed_Iowa_Vocational_Rehabilitation_Cases.csv", header=TRUE, stringsAsFactors=FALSE)
+IVRS_data <- read.csv(file.choose(), header=TRUE, stringsAsFactors=FALSE)
 
 # Initial split on comma to get City
 IVRS_data_transformed <- IVRS_data %>%
@@ -12,6 +13,13 @@ IVRS_data_transformed <- IVRS_data_transformed %>%
                           separate(Client.State.Zip.Code,c("Client.State","Client.Zip.Code"),sep="\\s+(?=\\S*$)")
 
 # Split Zip code into 5 digit format and extra information
+IVRS_data_transformed <- IVRS_data_transformed %>%
+                          separate(Client.Zip.Code,c("Client.Zip.Code","Client.Zip.Code.Extra.Info"),sep=5)
+
+# Remove '-' from extra zip code info
+IVRS_data_transformed$Client.Zip.Code.Extra.Info <- 
+  str_replace_all(IVRS_data_transformed$Client.Zip.Code.Extra.Info, "[^[:alnum:]]", "")
 
 
-write.csv(IVRS_data_transformed, "C:/Users/joelm/Documents/GitHub/DHR-Disabilities/Data Exploration/Datasets/Cleaned_Closed_Iowa_Vocational_Rehabilitation_Cases.csv")
+# write to file
+write.csv(IVRS_data_transformed, "C:/Users/joelm/Documents/GitHub/DHR-Disabilities/Data Exploration/Datasets/Closed_Iowa_Vocational_Rehabilitation_Cases.csv", row.names = FALSE)
