b6f46c9caac71fc73e546e6733b187abaa7360de
joelmar0 <joelmar@iastate.edu>
2022-07-13 09:26:14 -0500

    Update IVRS_Wage_Data.R

--- Data Exploration/R Projects/IVRS_Wage_Data.R
+++ Data Exploration/R Projects/IVRS_Wage_Data.R
@@ -5,10 +5,10 @@ library(dplyr)
 IVRS_data <- read.csv(file.choose(), header=TRUE, stringsAsFactors=FALSE, check.names = FALSE)
 
 # Get relevant fields
-IVRS_wage_data <- subset(IVRS_data, select = -c(4,7:10,16:18,21:25,27:31,34:35,37:38,45:46))
+IVRS_wage_data <- subset(IVRS_data, select = -c(4,7:10,18,21:25,27:31))
 
 # Reorder columns for easier readability
-IVRS_wage_data <- select(IVRS_wage_data, c(2,1,4,3,17:21,5:7,8:9,11,13,12,10,15:16,22,14))
+IVRS_wage_data <- select(IVRS_wage_data, c(2,1,4,3,23:27,5:8,13:14,9,15:16,10:12,17:22,28:30))
 
 # write to file
 write.csv(IVRS_wage_data, "C:/Users/joelm/Documents/GitHub/DHR-Disabilities/Data Exploration/Datasets/IVRS_Hourly_Wage.csv", row.names = FALSE)
